9d75a9005cac6c732867a38fdc183803462b4e33
herbsplit <boecker.sullivan@gmail.com>
2025-04-28 08:15:41 -0400

    getting headers and configuring results

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
 app.get('/button', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'button.html'));
 });
+
 // Function to get database entries
 async function getDatabase() {
   try {
